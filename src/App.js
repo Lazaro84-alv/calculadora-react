53a0cbb92cb4458755cd9d47db63a3e6332fa411
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-07-11 13:55:46 -0300

    Add images in App components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import KemetecLogo from "./imagenes/kemetec-logo.png";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="kemetec-logo-contenedor">
+        <img 
+          src={KemetecLogo}
+          className="kemetec-logo"
+          alt="Logo de kemetec"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+
+      </div>
     </div>
   );
 }
